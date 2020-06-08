a530244d0ac970c061473990b3fa4b58375fa6ad
mahalakshmichandrasekaran <maha.ranichandar@gmail.com>
2020-06-07 20:41:41 -0500

    Added testng.xml file, added build in pom.xml

--- src/main/java/com/EndToEndProject/PageBase/PageBase.java
+++ src/main/java/com/EndToEndProject/PageBase/PageBase.java
@@ -30,8 +30,8 @@ public class PageBase {
 
     }
 
-//   @AfterTest
-//   public void afterTests(){
-//        driver.quit();
-//  }
+   @AfterTest
+   public void afterTests(){
+        driver.quit();
+  }
 }
